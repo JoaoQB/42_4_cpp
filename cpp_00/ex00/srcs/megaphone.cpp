b4d269932ba0c29603da88a7ca9b597320e2c6b4
Joao Luis Queijo Barroco Sobral <jqueijo-@c2r4s6.42lisboa.com>
2025-01-06 16:38:04 +0000

    feat(ex00): megaphone done

---
+++ cpp_00/ex00/srcs/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jqueijo- <jqueijo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 16:02:11 by jqueijo-          #+#    #+#             */
+/*   Updated: 2025/01/06 16:08:38 by jqueijo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char **argv) {
+	std::string	str;
+
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	} else {
+		for (int i = 1; i < argc; i++) {
+			str = argv[i];
+			for (int i = 0; str[i]; i++) {
+				str[i] = std::toupper(str[i]);
+			}
+			std::cout << str;
+		}
+		std::cout << std::endl;
+	}
+}

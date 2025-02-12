f665a52dcba58c2c0c579c9402ccb62782d3a3d8
Joao Luis Queijo Barroco Sobral <jqueijo-@c2r8s9.42lisboa.com>
2025-02-12 17:31:37 +0000

    WIP: ex03

--- 03/ex03/srcs/DiamondTrap.cpp
+++ 03/ex03/srcs/DiamondTrap.cpp
@@ -6,10 +6,17 @@
 /*   By: jqueijo- <jqueijo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:31:22 by jqueijo-          #+#    #+#             */
-/*   Updated: 2025/02/12 15:33:55 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:57:00 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/DiamondTrap.hpp"
 
-
+DiamondTrap::DiamondTrap(std::string const &name) : ClapTrap(name + "_clap_name"),
+	FragTrap(name),
+	ScavTrap(name) {
+	this->name = name;
+	this->hitPoints = FragTrap::hitPoints;
+	this->energyPoints = ScavTrap::energyPoints;
+	this->attackDamage = FragTrap::attackDamage;
+}
